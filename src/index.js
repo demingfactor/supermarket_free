71958356a4e2750320e2327603964e34786f902d
Richie Khoo <evolve2k@gmail.com>
2018-04-16 21:24:43 +0930

    Moved all CSS files into index.js (So they are loaded as one file for speed)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 require('typeface-montserrat')
+import './assets/stylesheets/layout.css'
+import './assets/stylesheets/animations.css'
+import './assets/stylesheets/functions.css'
+import './assets/stylesheets/variables.css'
 import './styles.css'
 import './assets/stylesheets/identity.css'
 import './assets/stylesheets/lighter-youtube.css'
