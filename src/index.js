283f43d06e278fe45c8fc363ffb7313e061f1b69
Richie Khoo <evolve2k@gmail.com>
2018-04-16 17:31:57 +0930

    Added lighter-youtube for faster page loads. (Youtube is late loaded, made some modifications to remove all external image calls)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 require('typeface-montserrat')
 import './styles.css'
 import './assets/stylesheets/identity.css'
+import './assets/stylesheets/lighter-youtube.css'
+import './assets/javascripts/lighter-youtube.js'
